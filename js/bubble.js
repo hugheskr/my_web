220d8f81cd8590b24b34ba59aac06745ab44dbe7
Kris <kristopher_hughes@yahoo.com>
2016-05-09 12:34:39 -0700

    footer

--- js/bubble.js
+++ js/bubble.js
@@ -18,7 +18,7 @@ function widthChange(mq) {
     generateGraph(diameter);
   } else {
     // window width is less than 500px
-    diameter = 320;
+    diameter = 330;
     generateGraph(diameter);
   }
 }
