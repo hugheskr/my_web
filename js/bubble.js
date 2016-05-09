a196185c9fc00b1416dba530bcba2cc8da931467
Kris <kristopher_hughes@yahoo.com>
2016-05-09 13:46:43 -0700

    adjusted bubbles

--- js/bubble.js
+++ js/bubble.js
@@ -4,7 +4,7 @@ var diameter;
 
 // media query event handler
 if (matchMedia) {
-  var mq = window.matchMedia("(min-width: 500px)");
+  var mq = window.matchMedia("(min-width: 550px)");
   mq.addListener(widthChange);
   widthChange(mq);
 }
